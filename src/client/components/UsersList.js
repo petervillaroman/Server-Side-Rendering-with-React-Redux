5ab16430343d3ed7a02ce56572b05e7221485629
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-06-15 13:28:40 -0700

    renderUsers() and created path in Router
    We created a function called renderUsers in UsersList.js that gets the user name from our API and also routed the path "/users" to display this our UsersList component.
    
    Section: 6
    Lecture: 39

--- src/client/components/UsersList.js
+++ src/client/components/UsersList.js
@@ -6,9 +6,17 @@ class UsersList extends Component {
     componentDidMount() {
         // this.props.fetchUsers()
     }
+
+renderUsers() { 
+    return this.props.users.map(user => { 
+        return <li key={user.id}>{user.name}</li>
+    });
+}
+
     render() { 
         return (
             <div> Here's a big list of users:
+                <ul> {this.renderUsers()}</ul>
 
             </div>
         );
