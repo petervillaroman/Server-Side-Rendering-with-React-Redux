91a187d0b59bd91f2cc20362d4b5c16c264501bd
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-06-17 22:06:50 -0700

    62: Setting up Proxy
    In this commit we set up our server proxy using
    
    import proxy from 'express-http-proxy'.
    
    We are grabbing the cookie from the initial client request and attaching it to the request that AXIOS (in our renderer server) is making in order to trick the API into thinking that we are actually the client who made the request instead of AXIOM inside of our render server.
    
    Section 8
    Lecture 62

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
 /** @format */
 import 'babel-polyfill';
-import express from "express";
+import express from 'express';
 import { matchRoutes } from 'react-router-config';
-import Routes from './client/Routes'
-import renderer from "./helpers/renderer";
+import proxy from 'express-http-proxy';
+import Routes from './client/Routes';
+import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(
+	'/api',
+	proxy('http://react-ssr-api.herokuapp.com', {
+		proxyReqOptDecorator(opts) {
+			opts.header['x-forward-host'] = 'localhost:3000';
+			return opts;
+		},
+	})
+);
+app.use(express.static('public'));
 
-app.get("*", (req, res) => {
-  const store = createStore();
+app.get('*', (req, res) => {
+	const store = createStore();
 
-  const promises = matchRoutes(Routes, req.path).map(({route}) => {
-    return route.loadData ? route.loadData(store) : null;
-  });
+	const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+		return route.loadData ? route.loadData(store) : null;
+	});
 
-  Promise.all(promises).then(() =>  {
-    res.send(renderer(req,store));
-  });
-
-
+	Promise.all(promises).then(() => {
+		res.send(renderer(req, store));
+	});
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000...");
+	console.log('Listening on port 3000...');
 });
