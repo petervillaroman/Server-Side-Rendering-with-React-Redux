d942b3ba83dc989a74188f8f464198ccbeceb974
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2022-06-19 16:46:39 -0700

    Revert "64: Axios instances with redux thunk"
    This reverts commit df527d72e9f51842781905a2c82bf1e3191cbc6f.

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,11 +1,11 @@
-/** @format */
+import axios from 'axios';
 
 export const FETCH_USERS = 'fetch_users';
-export const fetchUsers = () => async (dispatch, getState, api) => {
-	const res = await api.get('/users');
+export const fetchUsers = () => async dispatch => {
+    const res = await axios.get('http://react-ssr-api.herokuapp.com/users')
 
-	dispatch({
-		type: FETCH_USERS,
-		payload: res,
-	});
+    dispatch( {
+        type: FETCH_USERS,
+        payload: res
+    });
 };
